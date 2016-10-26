fa717c1f4872cd8fe2cd2f4ab01145dfd9dbb17f
ovan66 <bstian23@gmail.com>
2016-10-25 21:00:16 -0300

    Aplicacion base

---
+++ app/src/main/java/cl/bastian/prueba1/CalculatorActivity.java
@@ -0,0 +1,13 @@
+package cl.bastian.prueba1;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class CalculatorActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_calculator);
+    }
+}
